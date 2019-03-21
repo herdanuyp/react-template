cc81ad1c546a649203fb9938007648cc22ca9a30
herdanu yudistira putra <heh.danu@gmail.com>
2019-03-21 16:02:06 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import SignIn from "./components/signIn";
+import Dashboard from "./components/dashboard/index";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
       </div>
     );
   }
