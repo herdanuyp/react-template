80bc4b6cf62cc0fa9e644937f4250534f69061e3
herdanu yudistira putra <heh.danu@gmail.com>
2019-03-21 16:03:22 +0700

    add dashboard homepage

---
+++ src/components/dashboard/index.js
@@ -0,0 +1,205 @@
+import React from "react";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MailIcon from "@material-ui/icons/Mail";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import { mainListItems } from "./listItems";
+
+const drawerWidth = 300;
+
+const styles = theme => ({
+  backgroundColor: {
+    background: "#FAFAFA"
+  },
+  root: {
+    display: "flex"
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
+  },
+  menuToolbarIcon: {
+    width: "100%",
+    textAlign: "center",
+    pointer: "pointer"
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 36
+  },
+  menuButtonHidden: {
+    display: "none"
+  },
+  title: {
+    flexGrow: 1
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing.unit * 9
+    }
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    height: "100vh",
+    overflow: "auto"
+  }
+});
+
+class Dashboard extends React.Component {
+  state = {
+    open: true,
+    anchorEl: null
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleProfileMenuOpen = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { anchorEl } = this.state;
+    const isMenuOpen = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="absolute"
+          className={classNames(
+            classes.appBar,
+            classes.backgroundColor,
+            this.state.open && classes.appBarShift
+          )}
+        >
+          <Toolbar
+            disableGutters={!this.state.open}
+            className={classes.toolbar}
+          >
+            <IconButton
+              aria-label="Open drawer"
+              onClick={this.handleDrawerOpen}
+              className={classNames(
+                classes.menuButton,
+                this.state.open && classes.menuButtonHidden
+              )}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              noWrap
+              className={classes.title}
+            >
+              Dashboard
+            </Typography>
+
+            <IconButton>
+              <Badge badgeContent={1} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge badgeContent={1} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              aria-owns={isMenuOpen ? "material-appbar" : undefined}
+              aria-haspopup="true"
+              onClick={this.handleProfileMenuOpen}
+            >
+              <AccountCircle />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classNames(
+              classes.drawerPaper,
+              !this.state.open && classes.drawerPaperClose
+            )
+          }}
+          open={this.state.open}
+        >
+          <div className={classes.toolbarIcon}>
+            <div
+              onClick={this.handleDrawerClose}
+              className={classes.menuToolbarIcon}
+            >
+              MENU
+            </div>
+            <IconButton onClick={this.handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <List>{mainListItems}</List>
+        </Drawer>
+        <main className={classes.content}>
+          
+        </main>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Dashboard);
---
+++ src/components/dashboard/listItems.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Home from '@material-ui/icons/Home';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import GroupAdd from '@material-ui/icons/GroupAdd';
+import AttachMoney from '@material-ui/icons/AttachMoney';
+import AccessTime from '@material-ui/icons/AccessTime';
+import Event from '@material-ui/icons/Event';
+import School from '@material-ui/icons/School';
+import AirplanemodeActive from '@material-ui/icons/AirplanemodeActive';
+import Money from '@material-ui/icons/Money';
+import AccountBalance from '@material-ui/icons/AccountBalance';
+import Check from '@material-ui/icons/Check';
+import Assignment from '@material-ui/icons/Assignment';
+import AssignmentReturned from '@material-ui/icons/AssignmentReturned';
+import Build from '@material-ui/icons/Build';
+
+export const mainListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <Home />
+      </ListItemIcon>
+      <ListItemText primary="Home" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AccountCircle />
+      </ListItemIcon>
+      <ListItemText primary="Employee Management" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <GroupAdd />
+      </ListItemIcon>
+      <ListItemText primary="Recruitment" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AttachMoney />
+      </ListItemIcon>
+      <ListItemText primary="Payroll" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AccessTime />
+      </ListItemIcon>
+      <ListItemText primary="Leave" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <Event />
+      </ListItemIcon>
+      <ListItemText primary="Training" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <School />
+      </ListItemIcon>
+      <ListItemText primary="Appraisal" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AirplanemodeActive />
+      </ListItemIcon>
+      <ListItemText primary="Travel" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <Money />
+      </ListItemIcon>
+      <ListItemText primary="Advance/Loans" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AccountBalance />
+      </ListItemIcon>
+      <ListItemText primary="Assests/Claims" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <Check />
+      </ListItemIcon>
+      <ListItemText primary="Attendance" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <Assignment />
+      </ListItemIcon>
+      <ListItemText primary="Task Management" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentReturned />
+      </ListItemIcon>
+      <ListItemText primary="Reports" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <Build />
+      </ListItemIcon>
+      <ListItemText primary="Settings" />
+    </ListItem>
+  </div>
+);
