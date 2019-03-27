d416470b7222c05ce578f4708d723574ee5a1d28
herdanu yudistira putra <heh.danu@gmail.com>
2019-03-27 11:07:46 +0700

    remove authenticated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import SignIn from "./components/signIn";
 import Dashboard from "./components/dashboard/index";
-import Callback from './components/callback';
-import Notfound from './components/notFound';
-import Auth from './Auth/auth';
-
-const auth = new Auth(); 
+import Callback from "./components/callback";
 
 class App extends Component {
   render() {
@@ -14,7 +10,7 @@ class App extends Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={SignIn} />
-          {!auth.isAuthenticated() ? <Route path="/dashboard" component={Dashboard} /> : <Route component={Notfound} />}
+          <Route path="/dashboard" component={Dashboard}/>}
           <Route path="/callback" component={Callback} />
         </Switch>
       </div>
