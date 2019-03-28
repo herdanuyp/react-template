eeab46d40891f94cd3fc7b824cfe494bd4287cc8
herdanu yudistira putra <heh.danu@gmail.com>
2019-03-28 13:51:24 +0700

    running test on sign in page

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -13,24 +13,30 @@ const styles = theme => ({
   },
 });
 
-function SignInPage(props) {
-  const { classes } = props;
-  const auth = new Auth();
+const auth = new Auth();
 
-  const login = () => {
+class SignInPage extends React.Component {
+
+  login = () => {
     auth.login();
   }
-
-  return (
-    <div className='sign-in-page'>
-      <Paper className={classes.root} elevation={4}>
-        <h1>This app is using Auth0 to sign in, please click button below to proceed</h1>
-      <Button variant="contained" color="secondary" className={classes.button} onClick={login}>
-        Sign In
-      </Button>
-      </Paper>
-    </div>
-  );
+  
+  render(){
+    const { classes } = this.props;
+    return (
+      <div className='sign-in-page'>
+        <Paper className={classes.root} elevation={4}>
+          <h1>This app is using Auth0 to sign in, please click button below to proceed</h1>
+        <Button variant="contained" color="secondary" className={classes.button} onClick={this.login}>
+          Sign In
+        </Button>
+        </Paper>
+      </div>
+    );
+  }
 }
+ 
+ 
+
 
 export default withStyles(styles)(SignInPage);
--- src/components/signIn.test.js
+++ src/components/signIn.test.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import SignInPage from './signIn';
-import {configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import SignInPage from "./signIn";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { createShallow, mount } from "@material-ui/core/test-utils";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
 
-configure({ adapter: new Adapter() })
-describe('<SignInPage />' , () => {
-    it('Should Render Sign In Page', () => {
-        const wrapper = shallow(<SignInPage />)
-    })
-})
+configure({ adapter: new Adapter() });
+
+describe("<SignInPage />", () => {
+  let shallow;
+  
+  beforeAll(() => {
+    shallow = createShallow({ dive: true });
+  });
+
+  it("Should Render Sign In Page", () => {
+    const wrapper = shallow(<SignInPage />);
+    console.log(wrapper.debug());
+  });
+
+  it("should render WithStyles(Paper)", () => {
+    const wrapper = shallow(<SignInPage />);
+    expect(wrapper.find(Paper)).toHaveLength(1);
+  });
+
+  it("should render WithStyles(Button)", () => {
+    const wrapper = shallow(<SignInPage />);
+    expect(wrapper.find(Button)).toHaveLength(1);
+  });
+
+//   it('should be clicked', () => {
+//     const wrapper = shallow(<SignInPage />);
+//     console.log(wrapper.find(Button).simulate('click'))
+//   })
+});
