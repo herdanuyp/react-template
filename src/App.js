07730c67326711e6f1743c23472cf4da9648fa5d
herdanu yudistira putra <heh.danu@gmail.com>
2019-04-01 16:48:54 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -3,32 +3,42 @@ import { Route, Switch, Router } from "react-router-dom";
 import SignInPage from "./components/signIn/signIn";
 import Dashboard from "./components/dashboard/index";
 import Callback from "./components/callback";
-import history from './history';
-import Auth from './Auth/auth';
+import history from "./history";
+import Auth from "./Auth/auth";
 
 const auth = new Auth();
 
-const handleAuthentication = ({location}) => {
+const handleAuthentication = ({ location }) => {
   if (/access_token|id_token|error/.test(location.hash)) {
     auth.handleAuthentication();
   }
-}
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
       <Router history={history}>
-        <Switch>
-          <Route exact path="/" render={(props) => <SignInPage auth={auth} {...props} /> } />
-          <Route path="/dashboard" render={(props) => <Dashboard auth={auth} {...props} />}/>
-          <Route path="/callback" render={(props) => {
-            handleAuthentication(props);
-            return <Callback {...props} />  
-          }} />
-        </Switch>
-        </Router>
-      </div>
+        <div className="App">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => <SignInPage auth={auth} {...props} />}
+            />
+            <Route
+              path="/dashboard"
+              render={props => <Dashboard auth={auth} {...props} />}
+            />
+            <Route
+              path="/callback"
+              render={props => {
+                handleAuthentication(props);
+                return <Callback {...props} />;
+              }}
+            />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
