125d6a0e04ae91f59fffa3a3436778d14de82a9a
herdanu yudistira putra <heh.danu@gmail.com>
2019-04-04 11:28:13 +0700

    change testing method using mount(enzyme)

--- src/components/home/index.test.js
+++ src/components/home/index.test.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import {configure, mount, shallow} from 'enzyme';
+import {configure, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createShallow } from "@material-ui/core/test-utils";
 import HomeMenu from './index';
 
 configure({ adapter: new Adapter() });
 
 describe("<HomeMenu />", () => {
-    let shallow;
-
-  beforeAll(() => {
-    shallow = createShallow({ dive: true });
-  });
-
     it("should render list menu in left side/side bar", () => {
-        const wrapper = shallow(<Router><HomeMenu /></Router>);
-        const deeperWrapper = wrapper.find(HomeMenu).dive().dive();
-        expect(deeperWrapper.find('div.left-list-items')).toHaveLength(1);
+        const wrapper = mount(<Router><HomeMenu /></Router>);
+        expect(wrapper.find('div.left-list-items')).toHaveLength(1);
     });
   });
