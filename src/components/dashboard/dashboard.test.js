f450f991a3abbd619811eb310bd524b2019e113f
herdanu yudistira putra <heh.danu@gmail.com>
2019-04-05 17:47:07 +0700

    rendered failed authentication page

--- src/components/dashboard/dashboard.test.js
+++ src/components/dashboard/dashboard.test.js
@@ -5,24 +5,32 @@ import { configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Dashboard from "./index";
 import { BrowserRouter as Router } from "react-router-dom";
-// import Auth from '../../Auth/auth';
-
-// const auth = new Auth();
 
 configure({ adapter: new Adapter() });
 
 describe("<Dashboard />", () => {
-  var mockAuth = {
+  const mockAuth = {
     isAuthenticated: () => true,
     login: jest.fn()
   };
+
+  const mockAuthFalse = {
+    isAuthenticated: () => false,
+    login: jest.fn()
+  };
+
+  const wrapperShallow = mount(<Dashboard auth={mockAuthFalse} />);
+  it("render not found page where authenticated is false", () => {
+    expect(
+      wrapperShallow.containsMatchingElement(<h1>You have to login first !</h1>)
+    ).toBeTruthy();
+  });
 
   const wrapper = mount(
     <Router>
       <Dashboard auth={mockAuth} />
     </Router>
   );
-
   it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(wrapper, div);
